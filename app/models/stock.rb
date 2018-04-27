f5a90f4aa7dc48d5365d239d00fc91900b588c95
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-04-27 16:14:34 +0300

    stocks search

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+  def self.search_for(ticker)
+    stock_quote = StockQuote::Stock.quote(ticker)
+    new(name: stock_quote.company_name, ticker: stock_quote.symbol, price: stock_quote.latest_price) if stock_quote
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
+
   devise_for :users
   root to: 'welcome#index'
 
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stocks', to: 'stocks#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
