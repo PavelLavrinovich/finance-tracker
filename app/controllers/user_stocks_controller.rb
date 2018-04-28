21a8c7e16ffd32fa77954ad89c9b493d55ddba2e
Pavel Lavrinovich <1avrinovich.pav31@gmail.com>
2018-04-28 13:45:15 +0300

    Untracking of the stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -8,4 +8,10 @@ class UserStocksController < ApplicationController
     UserStock.create(user: current_user, stock: stock)
     redirect_to my_portfolio_path
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    UserStock.where(user: current_user, stock: stock).first.destroy
+    redirect_to my_portfolio_path
+  end
 end
